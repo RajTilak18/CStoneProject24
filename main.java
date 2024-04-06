871fdb618746d8798acbb2eb9150a43221a9e94d
Raj Tilak <machyuthraj@gmail.com>
2024-04-06 17:21:12 +0530

    Modified main

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Addition " + Add.add(8, 5));
-        System.out.println("Substraction " + Substract.sub(6,1));
+        System.out.println("Subtract " + Substract.sub(6,1));
         System.out.println("Multiplication " + Multiplication.mul(2,9));
     }
 }
