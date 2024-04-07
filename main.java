b9d4b478a224b2b22be6cec29e9e1013f305bf98
Raj Tilak <machyuthraj@gmail.com>
2024-04-07 11:16:35 +0530

    Added code for division

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main {
         System.out.println("Addition " + Add.add(8, 5));
         System.out.println("Subtract " + Substract.sub(6,1));
         System.out.println("Multiplication " + Multiplication.mul(2,9));
+        System.out.println("Division " + Division.div(9,3));
     }
 }
